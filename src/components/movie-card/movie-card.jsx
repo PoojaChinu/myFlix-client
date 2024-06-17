0b4b741001884cb3e83ff5fbf3b85f7d7c763031
PoojaChinu <166599982+PoojaChinu@users.noreply.github.com>
2024-06-17 17:37:43 +0200

    Fetching data from Heroku

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,11 +1,19 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
-    return (
-      <div
-        onClick={() => {
-          onMovieClick(movie);
-        }}
-      >
-        {movie.Title}
-      </div>
-    );
-  };
+  return (
+    <div
+      onClick={() => {
+        onMovieClick(movie);
+      }}
+    >
+      {movie.Title}
+    </div>
+  );
+};
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    // Image: PropTypes.string.isRequired,
+  }).isRequired,
+};
